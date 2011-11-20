81e1ca76e0e3b71e7e1c89e00c32e182663cb8ef
Emmanuel Di Pretoro <edipretoro@gmail.com>
2011-11-20 12:26:21 +0100

    * bin/get_articles.pl: use Term::ProgressBar to show the progress to the user

--- bin/get_articles.pl
+++ bin/get_articles.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 
+use Term::ProgressBar;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 
@@ -15,9 +16,12 @@ my $pkm = myPKM::Schema->connect( $dsn );
 
 my $links_rs = $pkm->resultset('Link')->search( { content => 'not defined' } );
 
-my $total = $links_rs->count;
-print "We have $total links to process.\n";
+my $total = 0;
+my $progress = Term::ProgressBar->new( { 
+    name => 'Articles to collect (' . $links_rs->count . ')',
+    count => $links_rs->count
+});
 while (my $link = $links_rs->next()) {
     $link->update( { content => myPKM::get_article( $link->url() ) } );
-    print "Still ", $total--, " links to process.\n";
+    $progress->update(++$total);
 }

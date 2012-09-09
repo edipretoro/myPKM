550a90ba3964955715f69ebd2e4ec8c4785ff909
Emmanuel Di Pretoro <edipretoro@gmail.com>
2012-09-09 17:35:23 +0200

    * bin/app.pl: adding Plack::Builder to turn this Dancer app into a full fledge Plack app

--- bin/app.pl
+++ bin/app.pl
@@ -1,7 +1,20 @@
 #!/usr/bin/env perl
 use Dancer;
 use myPKM;
+use Plack::Builder;
 
 myPKM::deploy() if not myPKM::existing_database();
 
-dance;
+my $app = sub {
+    my $env     = shift;
+    my $request = Dancer::Request->new( env => $env );
+    Dancer->dance($request);
+};
+
+builder {
+    # enable 'Debug';
+    # enable 'Debug::DBITrace';
+    # enable 'Debug::DBIProfile', profile => 1;
+
+    $app;
+};

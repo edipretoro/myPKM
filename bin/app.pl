f21257873bd74a4b09bc5c6b9b4812956e0d4f4a
Emmanuel Di Pretoro <edipretoro@gmail.com>
2012-09-09 17:14:57 +0200

    * bin/mypkm_indexer.pl: turn the basic Dancer app into a full Plack by using Plack::Builder

--- bin/app.pl
+++ bin/app.pl
@@ -2,6 +2,20 @@
 use Dancer;
 use myPKM;
 
+use Plack::Builder;
+
 myPKM::deploy() if not myPKM::existing_database();
 
-dance;
+my $app = sub {
+    my $env     = shift;
+    my $request = Dancer::Request->new( env => $env );
+    Dancer->dance($request);
+};
+
+builder {
+    # enable 'Debug';
+    # enable 'Debug::DBITrace';
+    # enable 'Debug::DBIProfile', profile => 1;
+
+    $app;
+};

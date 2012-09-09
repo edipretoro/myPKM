b8fad341fc69d9c584c68aab553048c8e8c443cd
Emmanuel Di Pretoro <edipretoro@gmail.com>
2012-09-09 17:16:41 +0200

    Revert "* bin/mypkm_indexer.pl: turn the basic Dancer app into a full Plack by using Plack::Builder"
    This reverts commit f21257873bd74a4b09bc5c6b9b4812956e0d4f4a.
    Reverting "* bin/mypkm_indexer.pl: turn the basic Dancer app into a full Plack by using Plack::Builder"
    
    * bin/app.pl: turn the basic Dancer app into a full Plack by using Plack::Builder

--- bin/app.pl
+++ bin/app.pl
@@ -2,20 +2,6 @@
 use Dancer;
 use myPKM;
 
-use Plack::Builder;
-
 myPKM::deploy() if not myPKM::existing_database();
 
-my $app = sub {
-    my $env     = shift;
-    my $request = Dancer::Request->new( env => $env );
-    Dancer->dance($request);
-};
-
-builder {
-    # enable 'Debug';
-    # enable 'Debug::DBITrace';
-    # enable 'Debug::DBIProfile', profile => 1;
-
-    $app;
-};
+dance;
